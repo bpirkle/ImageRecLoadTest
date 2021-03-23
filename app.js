a95109ff274a23e508fbddaca73d963ca7b43aac
Bill Pirkle <bpirkle@wikimedia.org>
2021-03-23 11:45:03 -0500

    Command line argument support

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
 const imageRecLoadTest = require( './imageRecLoadTest' );
 const { PerformanceObserver, performance } = require('perf_hooks');
 
-const verbosity = 1; // larger values = more verbose output
-const numRequestsToMake = 1;
-const numPagesPerRequest = 1500;
+let verbosity = 1; // larger values = more verbose output
+let numRequestsToMake = 5;
+let numPagesPerRequest = 10;
 
 const perfObserver = new PerformanceObserver((items) => {
     items.getEntries().forEach((entry) => {
         console.log(`${entry.name}: ${entry.duration} ms`);
     })
 })
-  
+
+// Error handling here is minimal, but this is a single-purpose utility for use by devs.
+if (process.argv.length >= 3) {
+    numRequestsToMake = parseInt(process.argv[2]) || numRequestsToMake;
+}
+if (process.argv.length >= 4) {
+    numPagesPerRequest = parseInt(process.argv[3]) || numPagesPerRequest;
+}
+if (process.argv.length >= 5) {
+    verbosity = parseInt(process.argv[4]) || verbosity;
+}
+
+console.log(`numRequestsToMake: ${numRequestsToMake}, numPagesPerRequest: ${numPagesPerRequest}, verbosity: ${verbosity}`);
+
 perfObserver.observe({ entryTypes: ["measure"], buffer: true })
 
 performance.mark('app-start');
